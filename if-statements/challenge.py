418f27511608d70c2cb5d23f53770b631d3c00c7
Squidjigg <squidjigg80@outlook.com>
2020-12-02 22:40:23 +1100

    first commit

---
+++ if-statements/challenge.py
@@ -0,0 +1,9 @@
+answer = input('Would you like to continue? ')
+
+if(answer == 'yes' or answer == 'y'):
+    print('Continuing...')
+    print('Complete!')
+elif(answer == 'no' or answer == 'n'):
+    print('Exiting')
+else:
+    print('Please try again and respond with yes or no.')
