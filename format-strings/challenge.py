2da3e34dca99aa02444c1b96e5389881eef27729
Squidjigg <squidjigg80@outlook.com>
2020-12-03 20:19:38 +1100

    challenge completed

--- format-strings/challenge.py
+++ format-strings/challenge.py
@@ -10,7 +10,7 @@ sixth_value = 'sixth'
 first_value = first_value.lower()
 first_value = first_value.title()
 
-print(first_value.strip())
+print(first_value.rstrip().rjust(22))
 
 # Second challenge
 second_value = second_value.strip('-')
@@ -18,19 +18,22 @@ second_value = second_value.strip()
 
 print(second_value.capitalize())
 
-# Third challenge
+# Third challenge 
+third_value = third_value.swapcase()
+third_value = third_value.replace(' ','')
+third_value = third_value.replace('-',' ')
 
-print(third_value)
+print(third_value.rjust(31))
 
 # Fourth challenge - use only the print() function (no f-strings)
 print('#' + fourth_value, fifth_value, sixth_value, sep='#', end='!')
 
 # Fifth challenge - use only a single print() function.  Create tabs and new lines using f-strings.
-#print(f'')
+print(f'\n{fourth_value:>15}\n{fifth_value:>14}\n{sixth_value:>14}')
 
 
-### OUTPUT ###
-#First Challenge        
+### CHALLENGE OUTPUT ###
+#       First Challenge        
 #Second challenge
 #               Third challenge
 #fourth#fifth#sixth!
