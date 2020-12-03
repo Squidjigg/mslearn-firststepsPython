493c5cf6eb03346459a503378bf19b10aeac7123
Squidjigg <squidjigg80@outlook.com>
2020-12-03 15:35:16 +1100

    completed exercise3

---
+++ format-strings/exercise3.py
@@ -0,0 +1,27 @@
+#medicine = 'Coughussin'
+#dosage = 5
+#duration = 4.5
+
+#intstructions = '{} - Take {} ML by mouth every {} hours'.format(medicine, dosage, duration)
+#print(intstructions)
+
+#intstructions = '{2} - Take {1} ML by mouth every {0} hours'.format(medicine, dosage, duration)
+#print(intstructions)
+
+#intstructions = '{medicine} - Take {dosage} ML by mouth every {duration} hours'.format(medicine = 'Sneezergen', dosage = 10, duration = 6)
+#print(intstructions)
+
+#name = 'World'
+#message = f'Hello, {name}.'
+#print(message)
+
+#count = 10
+#value = 3.14
+#message = f'Count to {count}. Multiply by {value}.'
+#print(message)
+
+width = 5
+height = 10
+
+print(f'The parimeter is {(2 * width) + (2 * height)} and the area is {width * height}.')
+
