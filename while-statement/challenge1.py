f38cf25a4c5a70387feeaaf051c5f6cb84b9fafb
Squidjigg <squidjigg80@outlook.com>
2020-12-14 15:10:32 +1100

    added challenge1

---
+++ while-statement/challenge1.py
@@ -0,0 +1,16 @@
+import random
+
+guess = 0
+tries = 0
+actual = random.randint(1, 5)
+print(actual)
+
+while guess != actual:
+    guess = input('Guess an number between 1 and 5: ')
+    tries += 1
+
+    if guess.isnumeric():
+        guess = int(guess)
+        continue
+
+print(f'You guessed it in {tries}!')
