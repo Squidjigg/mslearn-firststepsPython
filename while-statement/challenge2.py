c62a6acc25ce66ee841521fbb942e96dead1762c
Squidjigg <squidjigg80@outlook.com>
2020-12-14 18:47:44 +1100

    completed challenge2

--- while-statement/challenge2.py
+++ while-statement/challenge2.py
@@ -3,7 +3,7 @@ import random
 guess = 0
 tries = 0
 value = random.randint(1, 10)
-print(value)                    # debug code
+#print(value)            # debug code
 
 print('Guess an number between 1 and 10\n')
 
@@ -13,8 +13,14 @@ while guess != value:
 
     if guess.isnumeric():
         guess = int(guess)
-        continue
     else:
         print('Numbers only, please!')
+        continue
+
+    if guess > value:
+        print('Your guess is to high, try again!')
+    elif guess < value:
+        print('You guess is to low, try again!')    
+
 else:
     print(f'You guessed it in {tries}!')
