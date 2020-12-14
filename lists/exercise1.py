02e99c45b4d5c4d40c3b2157aee0deb4df43939a
Squidjigg <squidjigg80@outlook.com>
2020-12-14 19:23:36 +1100

    added exercise1

---
+++ lists/exercise1.py
@@ -0,0 +1,58 @@
+colours = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'brown']
+
+# print(colours)
+# print(type(colours))
+
+# sundry = ['John', 3.14, 7, False]
+# print(sundry)
+# print(type(sundry))
+
+# print(f'0-based indexing into the list ... second item: {colours[1]}')
+
+# print(f'Last item of the list: {colours[-1]}')
+
+# print(f'Next to last item in the list: {colours[-2]}')
+
+# print('\nPrint a SLICE, starting at index 2 and excluding index 5:')
+# print(colours[2:5])
+# print(type(colours[2:5]))
+
+# print('\nPrint a slice, starting at index 0 to index 3:')
+# print(colours[:3])
+
+# print('\nPrint a slice, starting at index 4 to the end of the list:')
+# print(colours[4:])
+
+# print('\nPrint a slice, from the 4th from the end (but not the last item):')
+# print(colours[-4:-1])
+
+# colours.reverse()
+# print(colours)
+
+# colours.sort()
+# print(colours)
+
+# print(colours)
+
+# colour = colours.pop(0)
+# print('popped', colour)
+
+# print(colours)
+
+# print(colours)
+
+# colours.append('black')
+# colours.append('white')
+
+# colours.remove('yellow')
+# colours.remove('orange')
+ 
+# print(colours)
+
+# new_colours = ['lime', 'gray']
+# colours.extend(new_colours)
+# print(colours)
+
+print(colours)
+colours.clear()
+print(colours)
