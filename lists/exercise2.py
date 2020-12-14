a357d96d8232235cff34d7f8817558af3076fbae
Squidjigg <squidjigg80@outlook.com>
2020-12-14 22:55:15 +1100

    updated exercise2

--- lists/exercise2.py
+++ lists/exercise2.py
@@ -47,7 +47,7 @@
 # print(equipment)
 #---
 
-# suits = ['Hears', 'Spaces', 'Clubs', 'Diamonds']
+# suits = ['Hearts', 'Spaces', 'Clubs', 'Diamonds']
 # ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
 # for suit in suits:
