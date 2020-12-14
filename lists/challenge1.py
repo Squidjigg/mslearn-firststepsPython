e829669408614c9f39699b16de0028e08b2bcc6d
Squidjigg <squidjigg80@outlook.com>
2020-12-14 23:09:38 +1100

    updated challenge1

--- lists/challenge1.py
+++ lists/challenge1.py
@@ -1,3 +1,5 @@
+import random
+
 suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 cards = []
@@ -11,3 +13,11 @@ for suit in suits:
 
 #print(cards)                           # debug
 print(f'There are {len(cards)} cards in the deck')
+
+player_cards = random.choices(cards, k=5)
+cards_left = len(cards) - len(player_cards)
+
+print('\nDealing...\n')
+print(f'There are {cards_left} in the deck.')
+print('Player has the following cards in their hand:')
+print(player_cards)
